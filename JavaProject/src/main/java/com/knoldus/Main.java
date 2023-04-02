661bc5d2d3ce79cd7ed947fd4c0a8b93fac9d34f
riyaknol11 <riya.sharma@knoldus.com>
2023-04-02 17:13:40 +0530

    creating an object of Person class and displaying whether person is adult or not

--- JavaProject/src/main/java/com/knoldus/Main.java
+++ JavaProject/src/main/java/com/knoldus/Main.java
@@ -1,7 +1,21 @@
 package com.knoldus;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner takeInput=new Scanner(System.in);
+        System.out.println("Enter the name of person");
+        String name=takeInput.next();
+        System.out.println("Enter the age of person");
+        Integer age= takeInput.nextInt();
+
+        Person person =new Person(name,age);
+
+        System.out.println("name of the person: "+person.getName());
+        System.out.println("age of the person: "+person.getAge());
+
+        // calling the isAdult method to compare the age and displaying the result.
+        System.out.println("Is the person adult: "+person.isAdult());
     }
 }
