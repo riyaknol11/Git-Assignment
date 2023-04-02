8b689adb9f7fb7702abee8bab3d642945aa503bd
riyaknol11 <riya.sharma@knoldus.com>
2023-04-02 16:49:43 +0530

    added a person class with name and age atrributes

---
+++ JavaProject/src/main/java/com/knoldus/Person.java
@@ -0,0 +1,28 @@
+package com.knoldus;
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+   // getter and setter methods to get and set the name,age of person.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
